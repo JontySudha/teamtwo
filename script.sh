679021c740bc35b7b6be57444bf5195a735c07d2
Dinesh Sudha <102976767+JontySudha@users.noreply.github.com>
2026-07-14 16:07:11 +0530

    adding new feature to script

--- script.sh
+++ script.sh
@@ -6,5 +6,13 @@ docker run -d --name cont1 -p 100:80 img1
 docker tag img1 dineshsudha/img1:v2
 docker images
 
-while:
-    grep $0 | docker images
+while true; do
+    read -p "Enter image name: " image_name
+
+    if docker images | grep -wq "$image_name"; then
+        echo "✅ Image '$image_name' is present."
+        break
+    else
+        echo "❌ Image not found. Try again."
+    fi
+done
